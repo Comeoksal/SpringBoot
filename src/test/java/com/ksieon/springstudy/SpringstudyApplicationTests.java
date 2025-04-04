656f8a9dba7476a2173365b93b1329aaa9ff8d10
Ksieon <keumsiun0503@gmail.com>
2025-04-05 02:17:03 +0900

    :tada: Spring Study Start

---
+++ src/test/java/com/ksieon/springstudy/SpringstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksieon.springstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

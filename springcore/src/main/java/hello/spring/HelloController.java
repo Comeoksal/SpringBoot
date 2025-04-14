be18770c198f887ac7d34434306458c06e425308
Ksieon <keumsiun0503@gmail.com>
2025-04-14 23:58:56 +0900

    :recycle:Refactor: 수정

--- springcore/src/main/java/hello/spring/HelloController.java
+++ springcore/src/main/java/hello/spring/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello-spring")
+    @GetMapping("/hello-springs")
     public String hello() {
         System.out.println("hello Spring");
         return "Hello Spring";
